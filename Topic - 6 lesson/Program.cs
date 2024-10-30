9c1661345bb28f9ed770c455c05364428b78721a
Andre Mocko <339723967@GoTVDSB.ca>
2024-10-30 09:38:15 -0400

    Add project files.

---
+++ Topic - 6 lesson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic___6_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
